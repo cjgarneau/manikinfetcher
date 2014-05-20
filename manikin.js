315b9fc55db854a3bcbfced9224d15d97692a378
hauptdr <haupt.dr@gmail.com>
2014-05-20 11:42:50 -0400

    download dialogs
    download dialogs popup, but do not actually download anything yet. the
    dialogs include the images of the manikin and at able of the anthro.
    
    Also, if images are not displayed (i.e. there are more than 5 selected)
    the images and download buttons are opaque and buttons disabled.

--- manikin.js
+++ manikin.js
@@ -51,7 +51,7 @@ function initSliders(measures,totalDims){
 	var max1 = Math.ceil(Math.max.apply(Math,popData[measures[0]])/dataRound[measures[0]])*dataRound[measures[0]]
 	var slider_1_html = '';
 	slider_1_html += '<div data-role="rangeslider" id="Range1" >';
-	slider_1_html += '<label for="Range1-min" style="margin-bottom:-10px"><span id="SlideLabel1" style="font-size:20px;">'+fullName[measures[0]]+'</span> &nbsp;<span id="accom_1" style="color:blue; font-size:20px;">%</span></label>';
+	slider_1_html += '<label for="Range1-min" style="margin-bottom:-10px"><span id="SlideLabel1" style="font-size:20px;">'+fullName[measures[0]]+'</span> &nbsp;<span id="accom_1" style="color:#2e6794; font-size:20px;">%</span></label>';
 	slider_1_html += '<input type="range" name="Range1-min" id="Range1-min" value="'+min1+'" min="'+min1+'" max="'+max1+'" step="'+sliderStep[measures[0]]+'"/>';
 	slider_1_html += '<input type="range" name="Range1-max" id="Range1-max" value="'+max1+'" min="'+min1+'" max="'+max1+'" step="'+sliderStep[measures[0]]+'"/>';
 	slider_1_html += '</div>';
@@ -63,7 +63,7 @@ function initSliders(measures,totalDims){
 		var max2 = Math.ceil(Math.max.apply(Math,popData[measures[1]])/dataRound[measures[1]])*dataRound[measures[1]];
 		var slider_2_html='';
 		slider_2_html += '<div data-role="rangeslider" id="Range2" >';
-		slider_2_html += '<label for="Range2-min" style="margin-bottom:-10px"><span id="SlideLabel2" style="font-size:20px;">'+fullName[measures[1]]+'</span> &nbsp;<span id="accom_2" style="color:blue; font-size:20px;">%</span></label>';
+		slider_2_html += '<label for="Range2-min" style="margin-bottom:-10px"><span id="SlideLabel2" style="font-size:20px;">'+fullName[measures[1]]+'</span> &nbsp;<span id="accom_2" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_2_html += '<input type="range" name="Range2-min" id="Range2-min" value="'+min2+'" min="'+min2+'" max="'+max2+'" step="'+sliderStep[measures[1]]+'"/>';
 		slider_2_html += '<input type="range" name="Range2-max" id="Range2-max" value="'+max2+'" min="'+min2+'" max="'+max2+'" step="'+sliderStep[measures[1]]+'"/>';
 		$('#sliders').append(slider_2_html);
@@ -75,7 +75,7 @@ function initSliders(measures,totalDims){
 		var  max3 = Math.ceil(Math.max.apply(Math,popData[measures[2]])/dataRound[measures[2]])*dataRound[measures[2]];
 		var slider_3_html ='';
 		slider_3_html += '<div data-role="rangeslider" id="Range3" >';
-		slider_3_html += '<label for="Range3-min" style="margin-bottom:-10px"><span id="SlideLabel3" style="font-size:20px;">'+fullName[measures[2]]+'</span> &nbsp;<span id="accom_3" style="color:blue; font-size:20px;">%</span></label>';
+		slider_3_html += '<label for="Range3-min" style="margin-bottom:-10px"><span id="SlideLabel3" style="font-size:20px;">'+fullName[measures[2]]+'</span> &nbsp;<span id="accom_3" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_3_html += '<input type="range" name="Range3-min" id="Range3-min" value="'+min3+'" min="'+min3+'" max="'+max3+'" step="'+sliderStep[measures[2]]+'" />';
 		slider_3_html += '<input type="range" name="Range3-max" id="Range3-max" value="'+max3+'" min="'+min3+'" max="'+max3+'" step="'+sliderStep[measures[2]]+'" />';
 		$('#sliders').append(slider_3_html);
@@ -87,7 +87,7 @@ function initSliders(measures,totalDims){
 		var  max4 = Math.ceil(Math.max.apply(Math,popData[measures[3]])/dataRound[measures[3]])*dataRound[measures[3]];
 		var slider_4_html ='';
 		slider_4_html += '<div data-role="rangeslider" id="Range4" >';
-		slider_4_html += '<label for="Range4-min" style="margin-bottom:-10px"><span id="SlideLabel4" style="font-size:20px;">'+fullName[measures[3]]+'</span> &nbsp;<span id="accom_4" style="color:blue; font-size:20px;">%</span></label>';
+		slider_4_html += '<label for="Range4-min" style="margin-bottom:-10px"><span id="SlideLabel4" style="font-size:20px;">'+fullName[measures[3]]+'</span> &nbsp;<span id="accom_4" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_4_html += '<input type="range" name="Range4-min" id="Range4-min" value="'+min4+'" min="'+min4+'" max="'+max4+'" step="'+sliderStep[measures[3]]+'"/>';
 		slider_4_html += '<input type="range" name="Range4-max" id="Range4-max" value="'+max4+'" min="'+min4+'" max="'+max4+'" step="'+sliderStep[measures[3]]+'"/>';
 		$('#sliders').append(slider_4_html);
@@ -100,7 +100,7 @@ function initSliders(measures,totalDims){
 		var  max5 = Math.ceil(Math.max.apply(Math,popData[measures[4]])/dataRound[measures[4]])*dataRound[measures[4]];
 		var slider_5_html ='';
 		slider_5_html += '<div data-role="rangeslider" id="Range5" >';
-		slider_5_html += '<label for="Range5-min" style="margin-bottom:-10px"><span id="SlideLabel5" style="font-size:20px;">'+fullName[measures[4]]+'</span> &nbsp;<span id="accom_5" style="color:blue; font-size:20px;">%</span></label>';
+		slider_5_html += '<label for="Range5-min" style="margin-bottom:-10px"><span id="SlideLabel5" style="font-size:20px;">'+fullName[measures[4]]+'</span> &nbsp;<span id="accom_5" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_5_html += '<input type="range" name="Range5-min" id="Range5-min" value="'+min5+'" min="'+min5+'" max="'+max5+'" step="'+sliderStep[measures[4]]+'"/>';
 		slider_5_html += '<input type="range" name="Range5-max" id="Range5-max" value="'+max5+'" min="'+min5+'" max="'+max5+'" step="'+sliderStep[measures[4]]+'"/>';
 		$('#sliders').append(slider_5_html);
@@ -112,7 +112,7 @@ function initSliders(measures,totalDims){
 		var  max6 = Math.ceil(Math.max.apply(Math,popData[measures[5]])/dataRound[measures[5]])*dataRound[measures[5]];
 		var slider_6_html ='';
 		slider_6_html += '<div data-role="rangeslider" id="Range6" >';
-		slider_6_html += '<label for="Range6-min" style="margin-bottom:-10px"><span id="SlideLabel6" style="font-size:20px;">'+fullName[measures[5]]+'</span> &nbsp;<span id="accom_6" style="color:blue; font-size:20px;">%</span></label>';
+		slider_6_html += '<label for="Range6-min" style="margin-bottom:-10px"><span id="SlideLabel6" style="font-size:20px;">'+fullName[measures[5]]+'</span> &nbsp;<span id="accom_6" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_6_html += '<input type="range" name="Range6-min" id="Range6-min" value="'+min6+'" min="'+min6+'" max="'+max6+'" step="'+sliderStep[measures[5]]+'"/>';
 		slider_6_html += '<input type="range" name="Range6-max" id="Range6-max" value="'+max6+'" min="'+min6+'" max="'+max6+'" step="'+sliderStep[measures[5]]+'"/>';
 		$('#sliders').append(slider_6_html);
@@ -124,7 +124,7 @@ function initSliders(measures,totalDims){
 		var  max7 = Math.ceil(Math.max.apply(Math,popData[measures[6]])/dataRound[measures[6]])*dataRound[measures[6]];
 		var slider_7_html ='';
 		slider_7_html += '<div data-role="rangeslider" id="Range7" >';
-		slider_7_html += '<label for="Range7-min" style="margin-bottom:-10px"><span id="SlideLabel7" style="font-size:20px;">'+fullName[measures[6]]+'</span> &nbsp;<span id="accom_7" style="color:blue; font-size:20px;">%</span></label>';
+		slider_7_html += '<label for="Range7-min" style="margin-bottom:-10px"><span id="SlideLabel7" style="font-size:20px;">'+fullName[measures[6]]+'</span> &nbsp;<span id="accom_7" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_7_html += '<input type="range" name="Range7-min" id="Range7-min" value="'+min7+'" min="'+min7+'" max="'+max7+'" step="'+sliderStep[measures[6]]+'"/>';
 		slider_7_html += '<input type="range" name="Range7-max" id="Range7-max" value="'+max7+'" min="'+min7+'" max="'+max7+'" step="'+sliderStep[measures[6]]+'"/>';
 		$('#sliders').append(slider_7_html);
@@ -136,7 +136,7 @@ function initSliders(measures,totalDims){
 		var  max8 = Math.ceil(Math.max.apply(Math,popData[measures[7]])/dataRound[measures[7]])*dataRound[measures[7]];
 		var slider_8_html ='';
 		slider_8_html += '<div data-role="rangeslider" id="Range8" >';
-		slider_8_html += '<label for="Range8-min" style="margin-bottom:-10px"><span id="SlideLabel8" style="font-size:20px;">'+fullName[measures[7]]+'</span> &nbsp;<span id="accom_8" style="color:blue; font-size:20px;">%</span></label>';
+		slider_8_html += '<label for="Range8-min" style="margin-bottom:-10px"><span id="SlideLabel8" style="font-size:20px;">'+fullName[measures[7]]+'</span> &nbsp;<span id="accom_8" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_8_html += '<input type="range" name="Range8-min" id="Range8-min" value="'+min8+'" min="'+min8+'" max="'+max8+'" step="'+sliderStep[measures[7]]+'"/>';
 		slider_8_html += '<input type="range" name="Range8-max" id="Range8-max" value="'+max8+'" min="'+min8+'" max="'+max8+'" step="'+sliderStep[measures[7]]+'"/>';
 		$('#sliders').append(slider_8_html);
@@ -148,7 +148,7 @@ function initSliders(measures,totalDims){
 		var  max9 = Math.ceil(Math.max.apply(Math,popData[measures[8]])/dataRound[measures[8]])*dataRound[measures[8]];
 		var slider_9_html ='';
 		slider_9_html += '<div data-role="rangeslider" id="Range9" >';
-		slider_9_html += '<label for="Range9-min" style="margin-bottom:-10px"><span id="SlideLabel9" style="font-size:20px;">'+fullName[measures[8]]+'</span> &nbsp;<span id="accom_9" style="color:blue; font-size:20px;">%</span></label>';
+		slider_9_html += '<label for="Range9-min" style="margin-bottom:-10px"><span id="SlideLabel9" style="font-size:20px;">'+fullName[measures[8]]+'</span> &nbsp;<span id="accom_9" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_9_html += '<input type="range" name="Range9-min" id="Range9-min" value="'+min9+'" min="'+min9+'" max="'+max9+'" step="'+sliderStep[measures[8]]+'"/>';
 		slider_9_html += '<input type="range" name="Range9-max" id="Range9-max" value="'+max9+'" min="'+min9+'" max="'+max9+'" step="'+sliderStep[measures[8]]+'"/>';
 		$('#sliders').append(slider_9_html);
@@ -160,7 +160,7 @@ function initSliders(measures,totalDims){
 		var  max10 = Math.ceil(Math.max.apply(Math,popData[measures[9]])/dataRound[measures[9]])*dataRound[measures[9]];
 		var slider_10_html ='';
 		slider_10_html += '<div data-role="rangeslider" id="Range10" >';
-		slider_10_html += '<label for="Range10-min" style="margin-bottom:-10px"><span id="SlideLabel10" style="font-size:20px;">'+fullName[measures[9]]+'</span> &nbsp;<span id="accom_10" style="color:blue; font-size:20px;">%</span></label>';
+		slider_10_html += '<label for="Range10-min" style="margin-bottom:-10px"><span id="SlideLabel10" style="font-size:20px;">'+fullName[measures[9]]+'</span> &nbsp;<span id="accom_10" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_10_html += '<input type="range" name="Range10-min" id="Range10-min" value="'+min10+'" min="'+min10+'" max="'+max10+'" step="'+sliderStep[measures[9]]+'"/>';
 		slider_10_html += '<input type="range" name="Range10-max" id="Range10-max" value="'+max10+'" min="'+min10+'" max="'+max10+'" step="'+sliderStep[measures[9]]+'"/>';
 		$('#sliders').append(slider_10_html);
@@ -172,7 +172,7 @@ function initSliders(measures,totalDims){
 		var  max11 = Math.ceil(Math.max.apply(Math,popData[measures[10]])/dataRound[measures[10]])*dataRound[measures[10]];
 		var slider_11_html ='';
 		slider_11_html += '<div data-role="rangeslider" id="Range10" >';
-		slider_11_html += '<label for="Range11-min" style="margin-bottom:-10px"><span id="SlideLabel11" style="font-size:20px;">'+fullName[measures[10]]+'</span> &nbsp;<span id="accom_11" style="color:blue; font-size:20px;">%</span></label>';
+		slider_11_html += '<label for="Range11-min" style="margin-bottom:-10px"><span id="SlideLabel11" style="font-size:20px;">'+fullName[measures[10]]+'</span> &nbsp;<span id="accom_11" style="color:#2e6794; font-size:20px;">%</span></label>';
 		slider_11_html += '<input type="range" name="Range11-min" id="Range11-min" value="'+min11+'" min="'+min11+'" max="'+max11+'" step="'+sliderStep[measures[10]]+'"/>';
 		slider_11_html += '<input type="range" name="Range11-max" id="Range11-max" value="'+max11+'" min="'+min11+'" max="'+max11+'" step="'+sliderStep[measures[10]]+'"/>';
 		$('#sliders').append(slider_11_html);
@@ -202,8 +202,8 @@ function limitManikins(measures,totalDims){
 			}
 		
 		}			
-		//console.log(temp);
-		$('#accom_'+(m+1)).html('manikins selected: '+temp[measures[m]].length)
+
+		$('#accom_'+(m+1)).html('Manikins selected: '+temp[measures[m]].length)
 	}
 	manikinID=[];
 	var fullArray = [];
@@ -230,16 +230,21 @@ function getAnthro(manikinID){
 }
 
 function updateImages(){	
+	//check for front or side images
 	if($('#radio-choice-h-2b').prop('checked')){
 		var view ='side';
 	}else{
 		var view = 'front';
 	}
-	var num=5;
+	
+	var num=5;//default for viewing
 	var gridtype=['solo','a','b','c','d','e'];
+	//update number to display if less than 5
 	if(totalManikins<=5){
 		num=totalManikins;						
 		}
+		
+	//image update
 	var image_html=[];
 	for(i=0;i<num;i++){
 		image_html.push('<div class="ui-block-'+gridtype[i+1]+'" style="text-align:center" ><div class="ui-bar" style="text-align:center"><img src="./images/'+view+'/img_'+gender+'_'+view[0]+'_'+popData.STATURE[manikinID[i]]+'_'+Math.floor(popData.BMI[manikinID[i]])+'_'+Math.floor(popData.WAIST_CIRC_OMPHALION[manikinID[i]])+'.png" alt="human" style="height: 250px"></div></div>');
@@ -247,23 +252,49 @@ function updateImages(){
 	$('#imageView').html('');
 	$('#imageView').append('<div class="ui-grid-'+gridtype[num-1]+'"  id="images" style="text-align:center" >'+image_html.join('')+'</div>')
 	
+	//download bar update
 	var download_html=[];
 	for(j=0;j<num;j++){
-		download_html.push('<div class="ui-block-'+gridtype[j+1]+'"><a href="#popupDialog'+(j+1)+'" data-rel="popup" class="ui-shadow ui-btn ui-mini ui-corner-all">Download</a></div>');
+		if(totalManikins>5){
+			download_html.push('<div class="ui-block-'+gridtype[j+1]+'"><a href="#popupDialog'+(j+1)+'" id="#popupDialog'+(j+1)+'" data-rel="popup" class="ui-shadow ui-btn ui-state-disabled ui-mini ui-corner-all ">Download</a></div>');
+			$('#images').addClass('disable');
+			$('#downloadAll').html('<a href="#downloadAllDialog" data-rel="popup" class="ui-btn ui-mini ui-state-disabled" >Download All</a>')
+		}else{
+			download_html.push('<div class="ui-block-'+gridtype[j+1]+'"><a href="#popupDialog'+(j+1)+'" id="#popupDialog'+(j+1)+'" data-rel="popup" class="ui-shadow ui-btn ui-mini ui-corner-all">Download</a></div>');
+			$('#images').removeClass('disable');
+			$('#downloadAll').html('<a href="#downloadAllDialog" data-rel="popup" class="ui-btn ui-mini" >Download All</a>')			
+		}
 	}
 	$('#downloadbars').html('');
 	$('#downloadbars').append('<div class="ui-grid-'+gridtype[num-1]+'" id="downloads" >'+download_html.join('')+'</div>');
 	
-
+	//download dialog content update
 	var dialogs_html=[];
 	for(j=0;j<num;j++){
-		$('#dialogimg'+(j+1)+'').html('\
+		$('#dialogimg'+(j+1)).html('\
 			<img src="./images/front/img_'+gender+'_f_'+popData.STATURE[manikinID[j]]+'_'+Math.floor(popData.BMI[manikinID[j]])+'_'+Math.floor(popData.WAIST_CIRC_OMPHALION[manikinID[j]])+'.png" alt="human" style="height: 250px">\
 			<img src="./images/side/img_'+gender+'_s_'+popData.STATURE[manikinID[j]]+'_'+Math.floor(popData.BMI[manikinID[j]])+'_'+Math.floor(popData.WAIST_CIRC_OMPHALION[manikinID[j]])+'.png" alt="human" style="height: 250px">'
 		);
-	
+		
+		$('#anthroDialog'+(j+1)).html('<table>\
+			<tr><td>Stature</td><td>'+popData.STATURE[manikinID[j]]+' mm</td></tr>\
+			<tr><td>BMI</td><td>'+popData.BMI[manikinID[j]]+' kg/m<sup>2</sup></td></tr>\
+			<tr><td>SSH</td><td>'+popData.SSH[manikinID[j]]+'</td></tr>\
+			<tr><td>Biacromial Breadth</td><td>'+popData.BIACROMIAL_BRTH[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Knee Height-Sitting</td><td>'+popData.KNEE_HT_SITTING[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Forearm-Hand Length</td><td>'+popData.FOREARM_HAND_LGTH[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Sitting Hip Breadth</td><td>'+popData.HIP_BRDTH_SITTING[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Head Circumference</td><td>'+popData.HEAD_CIRC[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Chest Circumference</td><td>'+popData.CHEST_CIRC[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Waist Circumference</td><td>'+popData.WAIST_CIRC_OMPHALION[manikinID[j]]+' mm</td></tr>\
+			<tr><td>Hip Circumference (at buttocks)</td><td>'+popData.BUTTOCK_CIRC[manikinID[j]]+' mm</td></tr>\
+		</table>')
 	}
-
+	$('#downloadLink'+(j+1)).html('\
+		<a href="./images/front/img_'+gender+'_f_'+popData.STATURE[manikinID[j]]+'_'+Math.floor(popData.BMI[manikinID[j]])+'_'+Math.floor(popData.WAIST_CIRC_OMPHALION[manikinID[j]])+'.png" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-b" data-rel="back">Images (.png)</a>
+		<a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-b" data-rel="back" data-transition="flow">CAD (.jt)</a>
+		<a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-b" data-rel="back" data-transition="flow">Jack file (.fig)</a>		
+	);
 			
 }
 
@@ -487,12 +518,13 @@ $(document).on('change','#sliders',function(event){
 				imgChange = 1;
 				updateImages();
 				}
-		});			
+		});	
+		
+				
 	if (totalManikins>5 && imgChange==1) {
 		imgChange = 0;
 		updateImages();
 	}
-	
 });
 
 //update sliders from anthro
